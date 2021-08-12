b78b702b739af52bd4621d1ce6cf211e90cd350f
Nish021 <trisharaj021@gmail.com>
2021-08-12 18:19:01 +0530

    create reusable and editable input

--- src/components/dashboard/DashBoardToggle.js
+++ src/components/dashboard/DashBoardToggle.js
@@ -1,11 +1,19 @@
-import React from "react";
-import { Button, Drawer, Icon } from "rsuite";
+import React, { useCallback } from "react";
+import { Alert, Button, Drawer, Icon } from "rsuite";
 import Dashboard from ".";
 import { useMediaQuery, useModalState } from "../../misc/custom-hooks";
+import { auth } from "../../misc/firebase";
 
 const DashBoardToggle = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery("(max-width : 992px)");
+
+  //signout functionality
+  const onSignOut = useCallback(() => {
+    auth.signOut();
+    Alert.info("Signed out", 4000);
+    close();
+  }, [close]);
 
   return (
     <>
@@ -13,7 +21,7 @@ const DashBoardToggle = () => {
         <Icon icon="dashboard" /> DashboardToggle
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
-        <Dashboard />
+        <Dashboard onSignOut={onSignOut} />
       </Drawer>
     </>
   );
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Drawer, Button, Divider } from "rsuite";
+import { useProfile } from "../../context/profile.context";
+import EditableInput from "../EditableInput";
 
-const Dashboard = () => {
-  return <div>hello</div>;
+const Dashboard = ({ onSignOut }) => {
+  const { profile } = useProfile();
+
+  //on save input functionality
+  const onSave = async (newData) => {
+    console.log(newData);
+  };
+
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>hey, {profile.name}</h3>
+        <Divider />
+        <EditableInput
+          name="nickname"
+          initialValue={profile.name}
+          onSave={onSave}
+          label={<h6 className="mb-2">Nickname</h6>}
+        />
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
