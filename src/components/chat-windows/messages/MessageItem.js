1458cf10f56dcd24c23d89224c983045b66f59fd
Nish021 <trisharaj021@gmail.com>
2022-01-11 01:21:08 +0530

    handle delete messages operation

--- src/components/chat-windows/messages/MessageItem.js
+++ src/components/chat-windows/messages/MessageItem.js
@@ -10,7 +10,7 @@ import { auth } from "../../../misc/firebase";
 import { useHover, useMediaQuery } from "../../../misc/custom-hooks";
 import { useCurrentRoom } from "../../../context/current-room.context";
 
-const MessageItem = ({ message, handleAdmin, handleLike }) => {
+const MessageItem = ({ message, handleAdmin, handleLike, handleDelete }) => {
   const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
@@ -65,6 +65,15 @@ const MessageItem = ({ message, handleAdmin, handleLike }) => {
           onClick={() => handleLike(message.id)}
           badgeContent={likeCount}
         />
+
+        {isAuthor && (
+          <IconBtnControl
+            isVisible={canShowIcons}
+            iconName="close"
+            tooltip="Delete this message"
+            onClick={() => handleDelete(message.id)}
+          />
+        )}
       </div>
 
       <div>
